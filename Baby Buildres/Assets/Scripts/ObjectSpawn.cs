298c9d0fba2012967b6831f542536df84f454034
DESKTOP-J97QRER\Paul <yahtzee74>
2017-02-01 13:17:36 -0600

    Unity FIles Upload

---
+++ Baby Buildres/Assets/Scripts/ObjectSpawn.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSpawn : MonoBehaviour {
+
+    public GameObject[] objectPool;
+    public float timer;
+    float maxTime;
+    int index;
+	// Use this for initialization
+	void Start () {
+        maxTime = 0.5f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer += 1 * Time.deltaTime;
+        if(timer >= maxTime)
+        {
+            GameObject objectClone = Instantiate(objectPool[index], transform.position, transform.rotation);
+            timer = 0;
+            maxTime = Random.Range(1f, 2f);
+            index = Random.Range(0, objectPool.Length);
+        }
+	}
+}
